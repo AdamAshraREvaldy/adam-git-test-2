939c042ddfc0c80764764037b99256a4f37ac130
AdamAshraREvaldy <adamashra2@gmail.com>
2023-09-12 18:51:57 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste nostrum aperiam nihil, fuga assumenda neque libero error. Voluptates voluptate quod fuga placeat nostrum quos nesciunt, quo quaerat minima, neque ducimus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
